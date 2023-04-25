4037b6e3478b6f7d9545c34de6d2db676ad5516b
KaiKiat <92348692+Kaikiat1126@users.noreply.github.com>
2023-04-25 21:00:42 +0800

    feat: ✨main menu

--- DSTR_University_Recommendation_System.cpp
+++ DSTR_University_Recommendation_System.cpp
@@ -1,10 +1,23 @@
 // DSTR_University_Recommendation_System.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <string>
+#include "Message.h"
+#include "Menu.h"
+
+using namespace std;
+
+void runTest();
+
+void runTest()
+{
+    int option = Menu::mainMenu();
+    cout << "You choose: " << option << endl;
+}
 
 int main()
 {
-    std::cout << "Hello University Recommendation System!\n";
+    //std::cout << "Hello University Recommendation System!\n";
+	runTest();
 }
 
--- DateTime.h
+++ DateTime.h
@@ -7,29 +7,26 @@
 
 
 class DateTime {
-private:
-	static std::string getCurrentTime();
-	static std::string getCurrentDate();
 public:
 	static std::string getCurrentDateTime();
 };
 
-std::string DateTime::getCurrentTime() {
-	auto now = std::chrono::system_clock::now();
-	auto in_time_t = std::chrono::system_clock::to_time_t(now);
-	std::stringstream ss;
-	ss << std::put_time(localtime(&in_time_t), "%X");
-	return ss.str();
-}
-
-std::string DateTime::getCurrentDate() {
-	auto now = std::chrono::system_clock::now();
-	auto in_time_t = std::chrono::system_clock::to_time_t(now);
-	std::stringstream ss;
-	ss << std::put_time(localtime(&in_time_t), "%Y-%m-%d");
-	return ss.str();
-}
-
-std::string DateTime::getCurrentDateTime() {
-	return getCurrentDate() + " " + getCurrentTime();
-}
+//std::string DateTime::getCurrentTime() {
+//	auto now = std::chrono::system_clock::now();
+//	auto in_time_t = std::chrono::system_clock::to_time_t(now);
+//	std::stringstream ss;
+//	ss << std::put_time(localtime(&in_time_t), "%X");
+//	return ss.str();
+//}
+//
+//std::string DateTime::getCurrentDate() {
+//	auto now = std::chrono::system_clock::now();
+//	auto in_time_t = std::chrono::system_clock::to_time_t(now);
+//	std::stringstream ss;
+//	ss << std::put_time(localtime(&in_time_t), "%Y-%m-%d");
+//	return ss.str();
+//}
+//
+//std::string DateTime::getCurrentDateTime() {
+//	return getCurrentDate() + " " + getCurrentTime();
+//}

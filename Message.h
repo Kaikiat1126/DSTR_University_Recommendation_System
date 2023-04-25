4b84a88cde7fe4d402947b99f5dbd7ffdcd206ef
KaiKiat <92348692+Kaikiat1126@users.noreply.github.com>
2023-04-25 14:31:58 +0800

    feat: Message function

---
+++ Message.h
@@ -0,0 +1,36 @@
+#pragma once
+#include<iostream>
+#include<windows.h>
+
+
+class Message {
+private:
+	static void setColor(int fore, int back);
+public:
+	static void error(std::string message);
+	static void success(std::string message);
+	static void warning(std::string meesage);
+};
+
+void Message::setColor(int fore = 7, int back = 0)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (back << 4) + fore);
+}
+
+void Message::error(std::string message) {
+	setColor(4, 0);
+	std::cout << message << std::endl;
+	setColor();
+}
+
+void Message::success(std::string message) {
+	setColor(2, 0);
+	std::cout << message << std::endl;
+	setColor();
+}
+
+void Message::warning(std::string message) {
+	setColor(6, 0);
+	std::cout << message << std::endl;
+	setColor();
+}
